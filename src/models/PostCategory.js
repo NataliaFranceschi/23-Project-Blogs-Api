890e73389b033d77fd4c09f89b57441e7d0ca127
Natalia Franceschi <natalia.gfranceschi@gmail.com>
2022-11-30 19:16:08 -0300

    Requisito 11

---
+++ src/models/PostCategory.js
@@ -0,0 +1,30 @@
+const PostCategoryModel = (sequelize, DataTypes) => {
+    const PostCategory = sequelize.define('PostCategory', {
+      postId: DataTypes.INTEGER,
+      categoryId: DataTypes.INTEGER,
+     },
+    {
+        timestamps: false,
+        tableName: 'posts_categories',
+        underscored: true,
+      });
+    
+    PostCategory.associate = (models) => {
+        models.BlogPost.belongsToMany(models.Category, {
+          as: 'categories',
+          through: PostCategory,
+          foreignKey: 'postId', 
+          otherKey: 'categoryId', 
+        });
+        models.Category.belongsToMany(models.BlogPost, {
+          as: 'blog_posts',
+          through: PostCategory,
+          foreignKey: 'categoryId',  
+          otherKey: 'postId',
+        });
+      };
+  
+    return PostCategory;
+  };
+  
+module.exports = PostCategoryModel;
