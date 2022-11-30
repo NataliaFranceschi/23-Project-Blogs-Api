bccfa0f5d0862bd691d9d8658973604798dba138
Natalia Franceschi <natalia.gfranceschi@gmail.com>
2022-11-30 19:15:47 -0300

    Requisito 10

---
+++ src/models/BlogPost.js
@@ -0,0 +1,24 @@
+const BlogPostModel = (sequelize, DataTypes) => {
+    const BlogPost = sequelize.define('BlogPost', {
+        id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+        title: DataTypes.STRING,
+        content: DataTypes.STRING,
+        published: DataTypes.DATE,
+        updated: DataTypes.DATE,
+        userId: DataTypes.INTEGER,
+    },
+    {
+        timestamps: false,
+        tableName: 'blog_posts',
+        underscored: true,
+      });
+    
+    BlogPost.associate = (models) => {
+      BlogPost.belongsTo(models.User, 
+        { foreignKey: 'userId', as: 'users' });
+    };
+  
+    return BlogPost;
+  };
+  
+module.exports = BlogPostModel;
