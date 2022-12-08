5d08c762833caf8a08929465cab9519dd709a071
Natalia Franceschi <natalia.gfranceschi@gmail.com>
2022-12-08 15:44:53 -0300

    Retirando comentários

--- src/app.js
+++ src/app.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const router = require('./routers');
 
-// ...
-
 const app = express();
 
 app.use(express.json());
@@ -11,8 +9,5 @@ app.use('/user', router.userRouter);
 app.use('/login', router.loginRouter);
 app.use('/categories', router.categoryRouter);
 app.use('/post', router.postRouter);
-// ...
 
-// É importante exportar a constante `app`,
-// para que possa ser utilizada pelo arquivo `src/server.js`
 module.exports = app;
